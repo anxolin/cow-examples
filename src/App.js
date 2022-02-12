e27d4601905d2822d4be28c788ba6eb73f1e138d
Anxo Rodriguez <anxolin@gmail.com>
2022-02-12 09:52:04 -0700

    Get last 5 orders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { CowSdk } from '@gnosis.pm/cow-sdk'
+import { useEffect, useState } from 'react';
+
+const chainId = 1
+const cowSdk = new CowSdk(chainId)
+const EXPLORER_URL = 'https://explorer.cow.fi/orders'
 
 function App() {
+  const [orders, setOrders] = useState([])
+
+  useEffect(()=> {
+    cowSdk.cowApi.getOrders({
+      owner: '0x00000000005ef87f8ca7014309ece7260bbcdaeb', // Trader
+      limit: 5,
+      offset: 0
+    }).then(setOrders).catch(console.error)
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h3>Last 5 orders</h3>
+      <ul>
+        {orders ? orders.map(order => {
+          console.log(order)
+          return <li key={order.uid}><a href={EXPLORER_URL + '/' + order.uid} target="_blank" rel="noreferrer">{order.uid}</a></li>
+        }): 'No orders'}
+        
+      </ul>
+    </>
+  )
 }
 
 export default App;
