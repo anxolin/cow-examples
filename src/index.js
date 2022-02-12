0abfadf4c4105529f5becf15a1c36305425dcdfc
Anxo Rodriguez <anxolin@gmail.com>
2022-02-12 12:20:29 -0700

    Add roboto

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import "typeface-roboto";
 
 import reportWebVitals from './reportWebVitals';
 
